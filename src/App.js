3c72aef76af2c8d01a7cff3c212621e102e5d14f
zeeda <low.zhida@gmail.com>
2021-03-06 20:36:38 +0800

    React Components Part II

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponent'
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes'; //'./' roughly means to look for a folder with *foldername*, then look in folder with *filename*
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu /> {/*calling in Menu component */}
-    </div>
-  );
+class App extends Component {
+  constructor(props) { //rmb only class objs can use state 
+    super(props);
+
+    this.state={
+      dishes: DISHES
+    };
+  }
+  render() { //class...extends method must have render() {} structure inside
+    return (
+      <div>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/> 
+        {/*calling in Menu component + passing dishes state info as props to Menu component*/}
+      </div>
+    );
+  }
 }
-
-export default App;
+export default App; //export must put outside of the 'component' code 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,74 +1,63 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component { //component name is menu
     constructor(props) {
         super(props); //follow until here for basic template for creating a typical React component
-        this.state = {
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ],
-        };
+        this.state = { //only class componenets can store state info
+            selectedDish:null
+        }
+    }
+
+    onDishSelect(dish) {
+      this.setState({selectedDish:dish}); //rmb to alw use setState to change state value
+    }
+
+    renderDish(dish){
+      if (dish != null) {
+        return(
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+          </Card>
+        )
+      }
+      else {
+        return(
+          <div></div>
+        );
+      }
     }
 
     render() {
-        const menu = this.state.dishes.map((dish) => { 
-          //map is to iterate over each item in the js array; 
-          //dish is like python element loop iteration, each item is calles dish, 
-          //and will do the following operation for each 'dish' object
+        const menu = this.props.dishes.map((dish) => { 
+          /*map is to iterate over each item in the js array; 
+          dish is like python element loop iteration, each item is called 'dish', 
+          and will do the following operation for each 'dish' object*/
             return (
-              <div key={dish.id} className="col-12 mt-5"> 
-              {/*mt-5 is top margin of, every item given a unique element id (good practice for list objects). Curly braces means js object */}            
-                <Media tag="li"> {/* syntax for list creation */}
-                  <Media left middle> {/*adds 2 class atrributes to this item, class="medai-left media-middle" */}
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
+              <div key={dish.id} className="col-12 col-md-5 m-1"> {/*m-1 is 1u margin all around*/}
+              {/*mt-5 is top margin of, every item given a unique element id [compulsory for React?] (good practice for list objects). 
+              unique id useful for React to update any changes made. Curly braces means js object */}            
+                <Card onClick={() => this.onDishSelect(dish)}> {/* 'this' is pointing to the current 'dish' object card; passing the current selected dish to 'onDishSelect' function*/}
+                   <CardImg wdith="100%" src={dish.image} alt={dish.name} />
+                   <CardImgOverlay body className="ml-5">
+                    <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+                </Card>
               </div>
             );
         });
 
         return (
           <div className="container">
+            <div className="row">            
+                  {menu}              
+            </div>
             <div className="row">
-              <Media list>
-                  {menu}
-              </Media>
+              {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
