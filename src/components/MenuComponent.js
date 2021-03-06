a077718dd1a6f0044991b7b843928a6757806b0a
zeeda <low.zhida@gmail.com>
2021-03-06 17:30:47 +0800

    React Components Part I

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Menu extends Component { //component name is menu
+    constructor(props) {
+        super(props); //follow until here for basic template for creating a typical React component
+        this.state = {
+            dishes: [
+                {
+                  id: 0,
+                  name:'Uthappizza',
+                  image: 'assets/images/uthappizza.png',
+                  category: 'mains',
+                  label:'Hot',
+                  price:'4.99',
+                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
+               {
+                  id: 1,
+                  name:'Zucchipakoda',
+                  image: 'assets/images/zucchipakoda.png',
+                  category: 'appetizer',
+                  label:'',
+                  price:'1.99',
+                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
+               {
+                  id: 2,
+                  name:'Vadonut',
+                  image: 'assets/images/vadonut.png',
+                  category: 'appetizer',
+                  label:'New',
+                  price:'1.99',
+                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+               {
+                  id: 3,
+                  name:'ElaiCheese Cake',
+                  image: 'assets/images/elaicheesecake.png',
+                  category: 'dessert',
+                  label:'',
+                  price:'2.99',
+                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
+               ],
+        };
+    }
+
+    render() {
+        const menu = this.state.dishes.map((dish) => { 
+          //map is to iterate over each item in the js array; 
+          //dish is like python element loop iteration, each item is calles dish, 
+          //and will do the following operation for each 'dish' object
+            return (
+              <div key={dish.id} className="col-12 mt-5"> 
+              {/*mt-5 is top margin of, every item given a unique element id (good practice for list objects). Curly braces means js object */}            
+                <Media tag="li"> {/* syntax for list creation */}
+                  <Media left middle> {/*adds 2 class atrributes to this item, class="medai-left media-middle" */}
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Menu;
